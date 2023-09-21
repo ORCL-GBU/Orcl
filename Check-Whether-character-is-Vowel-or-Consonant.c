13a301aa2df0d5662ef9a3c21c1eeac322750a52
OMSAIRAM <78869875+Sampat55@users.noreply.github.com>
2023-09-21 13:53:10 +0530

    Dep & Check-Whether

---
+++ Check-Whether-character-is-Vowel-or-Consonant.c
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+/// printf("This programing implemented by @K SIVAIAH@ -- +91-9885553477")
+int main() {
+    char c;
+    bool isLowercaseVowel, isUppercaseVowel;
+
+    cout << "Enter an alphabet: ";
+    cin >> c;
+
+    // evaluates to 1 (true) if c is a lowercase vowel
+    isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+
+    // evaluates to 1 (true) if c is an uppercase vowel
+    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+
+    // show error message if c is not an alphabet
+    if (!isalpha(c))
+      printf("Error! Non-alphabetic character.");
+    else if (isLowercaseVowel || isUppercaseVowel)
+        cout << c << " is a vowel.";
+    else
+        cout << c << " is a consonant.";
+
+    return 0;
+}
+
+printf("WELCOME TO BENGALORE")
+Printf("Hello......")
